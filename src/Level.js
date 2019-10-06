ddd16f57225d2d5ac821fdf8e5492d426d3ac613
Joey Barajar <jbarajar@gmail.com>
2019-10-06 02:05:17 -0700

    added basic movement

--- src/Level.js
+++ src/Level.js
@@ -15,10 +15,79 @@ class Level extends React.Component {
                     1,0,3,0,4,1,
                     1,2,0,0,0,1,
                     1,0,0,0,0,1,
-                    1,1,1,1,1,1]
+                    1,1,1,1,1,1],
+            playerPos: 13,
+            levelWidth: 6
         }
 
         this.tile = [<Floor />, <Wall />, <Player />, <Box />, <Goal />]
+    }
+
+    moveRight(state) {
+        if ( state.tiles[state.playerPos + 1] === 0 ) {
+            let newTiles = Object.assign([], state.tiles)
+            newTiles[state.playerPos] = 0
+            newTiles[state.playerPos+1] = 2
+            return {
+                tiles: newTiles,
+                playerPos: state.playerPos+1
+            }
+        }
+    }
+
+    moveLeft(state) {
+        if ( state.tiles[state.playerPos - 1] === 0 ) {
+            let newTiles = Object.assign([], state.tiles)
+            newTiles[state.playerPos] = 0
+            newTiles[state.playerPos-1] = 2
+            return {
+                tiles: newTiles,
+                playerPos: state.playerPos-1
+            }
+        }
+    }
+
+    moveUp(state) {
+        if ( state.tiles[state.playerPos - state.levelWidth] == 0 ) {
+            let newTiles = Object.assign([], state.tiles)
+            newTiles[state.playerPos] = 0
+            newTiles[state.playerPos - state.levelWidth] = 2
+            return {
+                tiles: newTiles,
+                playerPos: state.playerPos - state.levelWidth
+            }
+        }
+    }
+
+    moveDown(state) {
+        if ( state.tiles[state.playerPos + state.levelWidth] == 0 ) {
+            let newTiles = Object.assign([], state.tiles)
+            newTiles[state.playerPos] = 0
+            newTiles[state.playerPos + state.levelWidth] = 2
+            return {
+                tiles: newTiles,
+                playerPos: state.playerPos + state.levelWidth
+            }
+        }
+    }
+
+    handleKeyDown(e) {
+        if (e.key === 'ArrowRight') {
+            this.setState(prevState => this.moveRight(prevState))
+        }
+        else if (e.key === 'ArrowLeft') {
+            this.setState(prevState => this.moveLeft(prevState))
+        }
+        else if (e.key === 'ArrowUp') {
+            this.setState(prevState => this.moveUp(prevState))
+        }
+        else if (e.key === 'ArrowDown') {
+            this.setState(prevState => this.moveDown(prevState))
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeyDown.bind(this))
     }
 
     render() {
--- src/Player.js
+++ src/Player.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import playerIcon from './player.png'
 import './Player.css'
 
 class Player extends React.Component {
     render() {
-         return <img className='player' src={playerIcon} alt='player'/>
+         return <div className='player'></div>
     }
 }
 
